ad6a2bf3f3598190f24e6335215ff3f85f1a0055
dev-gina <jaleeck1113@gmail.com>
2025-02-26 22:57:26 +0900

    프론트엔드 최종

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone", // ✅ Netlify에서 SSR 활성화
+  images: {
+    domains: ["example.com", "localhost"], 
+  },
 };
 
 module.exports = nextConfig;
