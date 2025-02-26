18695afcccdfbae1c85f6c6e53fbc67a79128ca1
dev-gina <jaleeck1113@gmail.com>
2025-02-26 22:21:09 +0900

    ffffinal

--- next.config.js
+++ next.config.js
@@ -1,28 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+  output: "standalone", // ✅ Netlify에서 SSR 활성화
   images: {
     domains: ["example.com", "localhost"], 
-  },
-  async rewrites() {
-    const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://final-back-gina.herokuapp.com";
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${API_BASE_URL}/api/:path*`, 
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/books/:path*", //여기만 비활성화
-        headers: [
-          { key: "Cache-Control", value: "no-store" },
-        ],
-      },
-    ];
   },
 };
 
