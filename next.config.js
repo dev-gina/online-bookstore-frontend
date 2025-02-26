be00dbb708b672fd20c645486fe540285e0ba4ff
dev-gina <jaleeck1113@gmail.com>
2025-02-26 21:41:02 +0900

    next.confing.js 수정

--- next.config.js
+++ next.config.js
@@ -1,18 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- reactStrictMode: true,
- swcMinify: true,
- images: {
-   domains: ["example.com", "localhost"], 
- },
- async rewrites() {
-   return [
-     {
-       source: "/api/:path*",
-       destination: "http://localhost:5001/api/:path*",
-     },
-   ];
- },
+  reactStrictMode: true,
+  swcMinify: true,
+  target: "server", // Netlify에서 SSR을 강제 활성화
+  images: {
+    domains: ["example.com", "localhost"], 
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NEXT_PUBLIC_API_BASE_URL + "/api/:path*", 
+      },
+    ];
+  },
+  experimental: {
+    outputStandalone: true, // etlify 배포 시 SSR이 제대로 동작하도록 설정
+  },
 };
 
 module.exports = nextConfig;
