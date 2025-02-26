a3c969c203b6988f1b3116223a0249e3b1915bd4
dev-gina <jaleeck1113@gmail.com>
2025-02-26 21:48:06 +0900

    next.config 최종

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  target: "server", // Netlify에서 SSR을 강제 활성화
+  output: "standalone",
   images: {
     domains: ["example.com", "localhost"], 
   },
@@ -13,9 +13,6 @@ const nextConfig = {
         destination: process.env.NEXT_PUBLIC_API_BASE_URL + "/api/:path*", 
       },
     ];
-  },
-  experimental: {
-    outputStandalone: true, // etlify 배포 시 SSR이 제대로 동작하도록 설정
   },
 };
 
